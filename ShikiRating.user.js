d6adbf99bbf4e25840a802ea81e4fdf4a94289d9
ImoutoChan <kirirrin@gmail.com>
2017-05-15 02:03:14 +0300

    v1.1.0

--- ShikiRating.user.js
+++ ShikiRating.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shiki Rating
 // @namespace    http://shikimori.org/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Rating from shiki users
 // @author       ImoutoChan
 // @match        http://shikimori.org/*
