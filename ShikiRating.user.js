856240cacd0fd4a206957a210a8a1c56377af5cb
ImoutoChan <kirirrin@gmail.com>
2017-05-15 03:08:04 +0300

    refactoring / v1.3.0

--- ShikiRating.user.js
+++ ShikiRating.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shiki Rating
 // @namespace    http://shikimori.org/
-// @version      1.2.0
+// @version      1.3.0
 // @description  Rating from shiki users
 // @author       ImoutoChan
 // @match        http://shikimori.org/*
@@ -20,20 +20,11 @@ var func = function() {
             return;
         }
 
-
-        var labelEngData = JSON.parse('{"0":"","1":"Worst Ever","2":"Terrible","3":"Very Bad","4":"Bad","5":"So-so","6":"Fine","7":"Good","8":"Excellent","9":"Great","10":"Masterpiece!"}');
-        var labelRusData = JSON.parse('{"0":"","1":"Хуже некуда","2":"Ужасно","3":"Очень плохо","4":"Плохо","5":"Более-менее","6":"Нормально","7":"Хорошо","8":"Отлично","9":"Великолепно","10":"Эпик вин!"}');
-        var currentLabel = $("div.b-rate > div.text-score > div.score-notice").last().text();
-        var useRus = _isContains(labelRusData, currentLabel);
-
-
         var newRate = $(".scores > .b-rate").clone();
         newRate.attr('id', 'shiki-score');
         newRate.appendTo(".scores");
 
-
         var scoreData = jQuery.parseJSON($("#rates_scores_stats").attr("data-stats"));
-
 
         var sumScore = 0;
         var totalCount = 0;
@@ -60,19 +51,13 @@ var func = function() {
         $("#shiki-score > div.stars-container > div.stars.score").attr('style', 'color: #456 !important;');
         $("#shiki-score > div.stars-container > div.stars.score").addClass("score-" + Math.round(shikiScore));
 
+        var labelData = $('body').attr('data-locale') === 'ru' ?
+                JSON.parse('{"0":"","1":"Хуже некуда","2":"Ужасно","3":"Очень плохо","4":"Плохо","5":"Более-менее","6":"Нормально","7":"Хорошо","8":"Отлично","9":"Великолепно","10":"Эпик вин!"}') :
+                JSON.parse('{"0":"","1":"Worst Ever","2":"Terrible","3":"Very Bad","4":"Bad","5":"So-so","6":"Fine","7":"Good","8":"Excellent","9":"Great","10":"Masterpiece!"}');
 
-        $("#shiki-score > div.text-score > div.score-notice").text(useRus ? labelRusData[Math.round(shikiScore)] : labelEngData[Math.round(shikiScore)]);
+        $("#shiki-score > div.text-score > div.score-notice").text(labelData[Math.round(shikiScore)]);
     }
 };
-
-function _isContains(json, value) {
-    let contains = false;
-    Object.keys(json).some(key => {
-        contains = typeof json[key] === 'object' ? _isContains(json[key], value) : json[key] === value;
-        return contains;
-    });
-    return contains;
-}
 
 $(document).bind('mouseup mousemove ready', function(){
     func();
