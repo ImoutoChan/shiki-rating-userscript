ad0b7880e7252ccd219768ac58abce2438206e00
ImoutoChan <kirirrin@gmail.com>
2018-02-02 23:44:46 +0300

    Update ShikiRating.user.js

--- ShikiRating.user.js
+++ ShikiRating.user.js
@@ -1,86 +1,141 @@
 // ==UserScript==
 // @name         Shiki Rating
 // @namespace    http://shikimori.org/
-// @version      1.6.2
+// @version      2.0
 // @description  Rating from shiki users
 // @author       ImoutoChan
 // @match        http://shikimori.org/*
 // @match        https://shikimori.org/*
-// @require 	 https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @license      MIT
 // @grant        none
 // ==/UserScript==
 
-var func = function() {
+var debug = false;
+
+function log(message) {
+    if (!debug) {
+        return;
+    }
+
+    console.log('ShikiRating: ' + message);
+}
+
+function getLocale() {
+    return document.querySelector('body').getAttribute('data-locale');
+}
+
+function needAddRating() {
+    return urlpart === "/animes" ||
+           urlpart === "/mangas" ||
+           urlpart === "/ranobe";
+}
+
+function removeLastClass(domElement) {
+    var classes = domElement.classList;
+    classes.remove(classes.item(classes.length - 1));
+}
+
+function appendShikiRating() {
     'use strict';
 
     var urlpart = window.location.pathname.substring(0,7);
-    if (urlpart == "/animes" || urlpart == "/mangas" || urlpart == "/ranobe")
-    {
-        if ($(".scores > .b-rate").length > 1)
-        {
-            return;
-        }
+    log(urlpart);
 
-        var newRate = $(".scores > .b-rate").clone();
-        newRate.attr('id', 'shiki-score');
-        newRate.appendTo(".scores");
-
-        var scoreData = jQuery.parseJSON($("#rates_scores_stats").attr("data-stats"));
-
-        var sumScore = 0;
-        var totalCount = 0;
-        for (var i = 0; i < scoreData.length; i++)
-        {
-            sumScore += scoreData[i].value * scoreData[i].name;
-            totalCount += scoreData[i].value;
-        }
-
-        var shikiScore = sumScore / totalCount;
-
-
-        for (i = 0; i < 10; i++)
-        {
-            $("#shiki-score > div.text-score > div.score-value.score-" + i).removeClass("score-" + i);
-        }
-        $("#shiki-score > div.text-score > div.score-value").html(shikiScore.toFixed(2));
-        $("#shiki-score > div.text-score > div.score-value").addClass("score-" + Math.round(shikiScore));
-
-        for (i = 0; i < 10; i++)
-        {
-            $("#shiki-score > div.stars-container > div.stars.score.score-" + i).removeClass("score-" + i);
-        }
-        $("#shiki-score > div.stars-container > div.stars.score").attr('style', 'color: #456 !important;');
-        $("#shiki-score > div.stars-container > div.stars.score").addClass("score-" + Math.round(shikiScore));
-
-        var labelData = $('body').attr('data-locale') === 'ru' ?
-                JSON.parse('{"0":"","1":"Хуже некуда","2":"Ужасно","3":"Очень плохо","4":"Плохо","5":"Более-менее","6":"Нормально","7":"Хорошо","8":"Отлично","9":"Великолепно","10":"Эпик вин!"}') :
-                JSON.parse('{"0":"","1":"Worst Ever","2":"Terrible","3":"Very Bad","4":"Bad","5":"So-so","6":"Fine","7":"Good","8":"Excellent","9":"Great","10":"Masterpiece!"}');
-
-        $("#shiki-score > div.text-score > div.score-notice").text(labelData[Math.round(shikiScore)]);
-        
-        if ($('body').attr('data-locale') === 'ru') {
-            $('.scores > .b-rate:eq(0)').append('<p class="score-source">На основе оценок mal</p>');
-        } else {
-            $('.scores > .b-rate:eq(0)').append('<p class="score-source">From MAL users</p>');
-        }
-
-        $('.scores > .b-rate:eq(1)').append('<p class="score-counter"><strong>' + totalCount + '</strong></p>');
-        if ($('body').attr('data-locale') === 'ru') {
-            $('.score-counter').prepend('На основе ').append(' оценок shiki');
-        } else {
-            $('.score-counter').prepend('From ').append(' shiki users');
-        }
-
-        $('.scores > .b-rate:eq(0)').attr('style', 'margin-bottom: 15px;');
-        $('.score-counter').attr('style', 'text-align: center; color: #7b8084;');
-        $('.score-source').attr('style', 'text-align: center; color: #7b8084;');
+    if (!needAddRating) {
+        log('wrong page');
+        return;
     }
-};
 
-$(document).bind('mouseup mousemove ready', function(){
-    func();
+    if (document.querySelector("#shiki-score") !== null) {
+        log('already created');
+        return;
+    }
+
+    if (document.querySelector(".scores > .b-rate") === null) {
+        log("can't find default rating");
+        return;
+    }
+
+    // get current rating element
+    var malRate = document.querySelector(".scores > .b-rate");
+    malRate.setAttribute('id', 'mal-score');
+
+    // clone it to new element
+    var newShikiRate = malRate.cloneNode(true);
+    newShikiRate.setAttribute('id', 'shiki-score');
+
+    // append cloned rating to parent container
+    var rateContainer = document.querySelector(".scores");
+    rateContainer.appendChild(newShikiRate);
+
+    // load scores stats
+    var scoreDataJson = document.querySelector("#rates_scores_stats").getAttribute("data-stats");
+    var scoreData = JSON.parse(scoreDataJson);
+    log(scoreDataJson);
+
+    // calculate shiki rating
+    var sumScore = 0;
+    var totalCount = 0;
+    for (var i = 0; i < scoreData.length; i++) {
+        sumScore += scoreData[i].value * scoreData[i].name;
+        totalCount += scoreData[i].value;
+    }
+    var shikiScore = sumScore / totalCount;
+    var shikiScoreDigit = Math.round(shikiScore);
+    log(shikiScore);
+
+    // set number value
+    var scoreElement = newShikiRate.querySelector("div.text-score > div.score-value");
+    scoreElement.innerHTML = shikiScore.toFixed(2);
+    removeLastClass(scoreElement);
+    scoreElement.classList.add("score-" + shikiScoreDigit);
+
+    // set stars calue
+    var starElement = newShikiRate.querySelector("div.stars-container > div.stars.score");
+    removeLastClass(starElement);
+    starElement.style.color = '#456';
+    starElement.classList.add("score-" + shikiScoreDigit);
+
+    // load labels
+    var labelData = getLocale() === 'ru' ?
+            {"0":"","1":"Хуже некуда","2":"Ужасно","3":"Очень плохо","4":"Плохо","5":"Более-менее","6":"Нормально","7":"Хорошо","8":"Отлично","9":"Великолепно","10":"Эпик вин!"} :
+            {"0":"","1":"Worst Ever","2":"Terrible","3":"Very Bad","4":"Bad","5":"So-so","6":"Fine","7":"Good","8":"Excellent","9":"Great","10":"Masterpiece!"};
+
+    // set label under score
+    newShikiRate.querySelector("div.text-score > div.score-notice").textContent = labelData[shikiScoreDigit];
+
+    // set mal description label
+    var malLabel = getLocale() === 'ru' ? 'На основе оценок mal' : 'From MAL users';
+    malRate.insertAdjacentHTML('afterend', '<p class="score-source">' + malLabel + '</p>');
+
+    // set shiki description label
+    var shikiCountLabel = '<strong>' + totalCount + '</strong>';
+    shikiCountLabel = (getLocale() === 'ru')
+        ? 'На основе ' + shikiCountLabel + ' оценок shiki'
+        : 'From ' + shikiCountLabel + ' shiki users';
+    newShikiRate.insertAdjacentHTML('afterend', '<p class="score-counter">' + shikiCountLabel + '</p>');
+
+    // set style for mal description label
+    var malScoreLabelElement = document.querySelector('.score-source');
+    malScoreLabelElement.style.marginBottom = '15px';
+    malScoreLabelElement.style.textAlign = 'center';
+    malScoreLabelElement.style.color = '#7b8084';
+
+    // set shiki for mal description label
+    var shikiScoreLabelElement = document.querySelector('.score-counter');
+    shikiScoreLabelElement.style.textAlign = 'center';
+    shikiScoreLabelElement.style.color = '#7b8084';
+}
+
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+    document.addEventListener('page:load', fn);
+  }
+}
+
+ready(function() {
+    appendShikiRating();
 });
-
-$(document).ready(func);
-$(document).on('page:load', func);
-$(document).on('turbolinks:load', func);
