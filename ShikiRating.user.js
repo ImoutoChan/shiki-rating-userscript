a6575f657fcae341b96ea1a245229aaa3f569837
ImoutoChan <kirirrin@gmail.com>
2026-01-31 03:57:58 +0500

    Bump version to 2.9.0

--- ShikiRating.user.js
+++ ShikiRating.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shiki Rating
 // @namespace    http://shikimori.org/
-// @version      2.8.3
+// @version      2.9.0
 // @description  Rating from shiki users
 // @author       ImoutoChan
 // @match        http://shikimori.org/*
