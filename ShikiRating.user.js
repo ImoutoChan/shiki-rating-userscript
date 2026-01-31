3684562b4213a3e8f64a39632c5d87710a59c367
Konstantin Baranov <gh@seig.ru>
2026-01-31 09:41:36 +0300

    Bump version to 3.0.0

--- ShikiRating.user.js
+++ ShikiRating.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shiki Rating
 // @namespace    http://shikimori.org/
-// @version      2.9.0
+// @version      3.0.0
 // @description  Rating from shiki users
 // @author       ImoutoChan
 // @match        http://shikimori.org/*
